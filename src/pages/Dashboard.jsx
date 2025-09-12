2231c69c1421041d4875f460588fbc2f5cc8d223
Taha38432u <rasheedtaha111@gmail.com>
2025-09-12 15:31:56 +0500

    fixed resonsive dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,15 +2,18 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import DashboardFilter from "../features/dashboard/DashboardFilter.jsx";
 import DashboardLayout from "../features/dashboard/DashboardLayout.jsx";
+import { useMediaQuery } from "react-responsive";
 
 function Dashboard() {
+  // Define a breakpoint for mobile
+  const isMobile = useMediaQuery({ maxWidth: 768 }); // adjust width as needed
+
   return (
     <>
-      <Row type="vertical">
+      <Row type={isMobile ? "vertical" : "horizontal"}>
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
-
       <DashboardLayout />
     </>
   );
